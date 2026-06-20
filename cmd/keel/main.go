ca6e4d08300c1b18be391529d254ea8a4e92e6b4
Bhuvanesh Prasad <bhuvaneshprasad10@gmail.com>
2026-06-20 10:50:54 +0530

    add health endpoint

--- cmd/keel/main.go
+++ cmd/keel/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"log"
+	"encoding/json"
+)
+
+func healthHandler(w http.ResponseWriter, r *http.Request){
+	response := map[string]string{
+		"status": "ok",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if err := json.NewEncoder(w).Encode(response); err!=nil{
+		http.Error(w, "internal server error", http.StatusInternalServerError)
+	}
+
+}
 
 func main() {
-	fmt.Println("Starting Keel...")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /health", healthHandler)
+
+	log.Println("Keel starting on :3000")
+
+	if err := http.ListenAndServe(":3000", mux); err != nil {
+		log.Fatal(err)
+	}
 }
